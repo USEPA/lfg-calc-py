82a928ddf5f51d8d52642e56c388eba09b39634f
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-07-14 10:49:15 -0600

    update import statement

--- lfg_calc_py/lfg_log.py
+++ lfg_calc_py/lfg_log.py
@@ -2,7 +2,7 @@ import logging
 import shutil
 import sys
 from esupy.processed_data_mgmt import mkdir_if_missing
-from .settings import logoutputpath
+from lfg_calc_py.settings import logoutputpath
 
 try:
     from colorama import init, Fore, Style
