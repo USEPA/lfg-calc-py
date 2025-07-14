a98c7d7ea9e56764f4a4a93d6c70cdf11440a949
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-07-14 10:46:48 -0600

    add example method

--- examples/emission_calculations.py
+++ examples/emission_calculations.py
@@ -1,3 +1,10 @@
 """
 Examples for generating landfill emissions
 """
+from lfg_calc_py import getLFGCalculations
+
+
+methodname = "Landfill_Example_Material_Specific"
+
+df = getLFGCalculations(methodname)
+
