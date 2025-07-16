65fcf25e219acc465c5801ddaa4eecb4c3cec83a
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-07-15 20:26:54 -0600

    update example to always generate the method

--- examples/emission_calculations.py
+++ examples/emission_calculations.py
@@ -2,9 +2,16 @@
 Examples for generating landfill emissions
 """
 from lfg_calc_py import getLFGCalculations
+from lfg_calc_py import LFG
 
 
 methodname = "Landfill_Example_Material_Specific"
 
-df = getLFGCalculations(methodname)
+# load locally or generate LFG emissions if do not exist locally
+# df = getLFGCalculations(methodname)
+
+# generate LFG emissions
+df = LFG.generateLFG(methodname)
+
+
 
