ba5ba3b892658d1d8f598b6b078803564812acc9
catherinebirney <60186515+catherinebirney@users.noreply.github.com>
2025-08-15 16:41:47 -0600

    update example file

--- examples/emission_calculations.py
+++ examples/emission_calculations.py
@@ -7,11 +7,8 @@ from lfg_calc_py import LFG
 
 methodname = "Landfill_Example_Single_Year_Acceptance"
 
-# load locally or generate LFG emissions if do not exist locally
-# df = getLFGCalculations(methodname)
+# load locally or generates LFG emissions if does not exist locally
+df_load = getLFGCalculations(methodname)
 
-# generate LFG emissions
-df = LFG.generateLFG(methodname)
-
-
-
+# optionally, can generate the method, will replace a local file
+df_generate = LFG.generateLFG(methodname)
